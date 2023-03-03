86cfa9ba97570b6ee40d5691d63dbdd0acc85e8a
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2023-03-03 22:33:04 +0100

    ejercicios del tema 4 terminados

---
+++ introduccion_programacion/tema_4/ejercicio_tema4.js
@@ -0,0 +1,59 @@
+/*
+###########################
+##EJERCICIO TEMA 4#########
+## HECHO EN JAVASCRIPT#####
+###########################
+*/
+
+
+// if
+let numeroIf = 9;
+if (numeroIf > 0) {
+  console.log("El número es positivo.");
+} else if (numeroIf < 0) {
+  console.log("El número es negativo.");
+} else {
+  console.log("El número es cero.");
+}
+
+// while
+let numeroWhile = 0;
+while (numeroWhile < 3) {
+  numeroWhile++;
+  console.log(`Número en while ${numeroWhile}`);
+}
+
+// dowhile
+let numeroDoWhile = 0;
+do {
+  numeroDoWhile++;
+  if (numeroDoWhile === 2) {
+      console.log(`Número en Dowhile ${numeroDoWhile}`);
+    
+  }
+} while (numeroDoWhile < 3);
+
+// Bucle for
+for (let numeroFor = 0; numeroFor <= 3; numeroFor++) {
+  console.log(`Número en for ${numeroFor}`);
+}
+
+// Switch
+let estacion = "verano";
+switch (estacion) {
+  case "primavera":
+    console.log("Estamos en primavera.");
+    break;
+  case "verano":
+    console.log("Estamos en verano.");
+    break;
+  case "otoño":
+    console.log("Estamos en otoño.");
+    break;
+  case "invierno":
+    console.log("Estamos en invierno.");
+    break;
+  default:
+    console.log("upppppssssss solo estaciones");
+    break;
+}
